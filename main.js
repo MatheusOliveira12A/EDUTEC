1f62e845ba97e1a379beac2560c6b6a8882e7b48
matheusnoliveira09 <matheusnoliveira09@gmail.com>
2025-09-07 22:20:47 +0000

    finalização do css

---
+++ main.js
@@ -0,0 +1,17 @@
+<script>
+
+document.addEventListener("DOMContentLoaded", function() {
+  const logo = document.getElementById("logo");
+  if (logo) {
+    logo.addEventListener("click", function() {
+      window.location.href = "=, sobre_nos.html";
+    })
+    if (logo) {
+      this.onclick = function() {
+        window.location.href = "index.html";
+      }
+    }
+  }
+});
+  </script>
+
